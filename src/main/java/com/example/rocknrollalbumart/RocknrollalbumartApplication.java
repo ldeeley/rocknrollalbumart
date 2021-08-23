9986573c8fda713d43f7b03b7ed33961ce3af4d2
Lester Deeley <lester.deeley@yahoo.com>
2021-08-23 11:10:51 +0100

    Initial commit

---
+++ src/main/java/com/example/rocknrollalbumart/RocknrollalbumartApplication.java
@@ -0,0 +1,13 @@
+package com.example.rocknrollalbumart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocknrollalbumartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocknrollalbumartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rocknrollalbumart/RocknrollalbumartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rocknrollalbumart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocknrollalbumartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
